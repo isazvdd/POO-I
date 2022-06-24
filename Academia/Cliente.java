9d1603d9f798ffd8dfc261eee63edc0c25126a79
Isadora Azevedo <92753238+isazvdd@users.noreply.github.com>
2022-06-23 21:38:20 -0300

    Update Cliente.java

--- Academia/Cliente.java
+++ Academia/Cliente.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cliente {
     private String nome;
@@ -69,15 +71,120 @@ public class Cliente {
         return soma/cont;
     }
     
-    //       FAZER UM IMC DE CLIENTES SEM UTILIZAR O FOR OU WHILE, UTILIZANDO A STREAM
+    
+    public List<Metrica> ordenarMetricas(){
+        this.metricas = new ArrayList<Metrica>(metricas.stream().sorted((d1, d2)->d1.getData().compareTo(d2.getData())).collect(Collectors.toList()));
+        return metricas.stream().sorted((d1, d2)->d1.getData().compareTo(d2.getData())).collect(Collectors.toList());
+    }
+   
+    
+    public Metrica pesquisarMetrica(String data) {
+        return metricas.stream().filter(m->m.getData().equals(data)).findFirst().orElse(null);
+    }
+    
+    public boolean adicionarMetrica(double altura, double peso, double cintura, double biceps, double peito, String data){
+        if(pesquisarMetrica(data)==null){
+            Metrica m = new Metrica();
+            m.setAltura(altura); 
+            m.setPeso(peso); 
+            m.setCintura(cintura); 
+            m.setBiceps(biceps); 
+            m.setPeito(peito); 
+            m.setData(data); 
+            metricas.add(m);
+            
+            return true;
+        }
+        return false;
+    }   
+        
+    public boolean removerMetrica(String data){
+        Metrica m = pesquisarMetrica(data);
+        if(m!=null){
+            metricas.remove(m);
+            return true;    
+        }
+        return false;
+    }
+    
     public double imcAtual(){
         return metricas.get(metricas.size() -1).imc();
     }
-        
-    public Metrica metricaAtual(){
-        return metricas.get(metricas.size() -1);
+  
+    public double pesoAtual()
+    {
+        return metricaAtual().getPeso();
     }
-        
+    
+    public double alturaAtual()
+    {
+        return metricaAtual().getAltura();
+    }
+    
+    public double bicepsAtual()
+    {
+        return metricaAtual().getBiceps();
+    }
+    
+    public double peitoralAtual()
+    {
+        return metricaAtual().getPeito();
+    }
+    
+    public double pesoInicial()
+    {
+        return metricaInicial().getPeso();
+    }
+
+    public double alturaInicial()
+    {
+        return metricaInicial().getAltura();
+    
+    }
+    
+    public double bicepsInicial()
+    {
+        return metricaInicial().getBiceps();
+    }
+    
+    public double peitoralInicial()
+    {
+        return metricaInicial().getPeito();
+    }
+    
+    public double pesoVariacao()
+    {
+        return pesoAtual() - pesoInicial();
+    }
+    
+    public double alturaVariacao()
+    {
+        return alturaAtual() - alturaInicial();
+    }
+    
+    public double bicepsVariacao()
+    {
+        return bicepsAtual() - bicepsInicial();
+    }
+    
+    public double peitoralVariacao()
+    {
+        return peitoralAtual() - peitoralInicial();
+    }
+    
+    public Metrica metricaAtual()
+    {
+        ordenarMetricas();
+        return metricas.get(metricas.size()-1);
+    }
+    
+    public Metrica metricaInicial()
+    {
+        ordenarMetricas();
+        return metricas.get(0);
+    }
+    
+                 
     public String adequacao(){
         if(imcAtual() < 18.5){
             return "IMC abaixo do peso";
